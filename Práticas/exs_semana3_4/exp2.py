ae3dfa2a1526e6eb86b66792c87a01135d2474ae
Ricardo Nunes <ricardo.nunes.1892@gmail.com>
2022-04-11 19:37:13 +0000

    Added T and tried to do 20

--- Práticas/exs_semana3_4/exp2.py
+++ Práticas/exs_semana3_4/exp2.py
@@ -14,17 +14,29 @@ img_gray = cv.GaussianBlur(img_gray, (5, 5), 0)
 _, thresh = cv.threshold(
     img_gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
-kernel = np.ones((3, 3), np.uint8)
-opening = cv.morphologyEx(thresh, cv.MORPH_CLOSE, kernel, iterations=4)
+closed = cv.morphologyEx(thresh, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (7, 7)))
 
-element = cv.getStructuringElement(cv.MORPH_RECT, (1, 5) )
+opened = cv.morphologyEx(closed, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (38, 38)), iterations=1)
+# opened2 = cv.morphologyEx(opened, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_ELLIPSE, (38, 38)), iterations=1)
 
-erosion = cv.erode(opening, element, iterations=3)
+opened = cv.erode(opened, cv.getStructuringElement(cv.MORPH_ELLIPSE, (30, 30)), iterations=1)
 
-cv.imshow("opening", opening)
+n, labels, stats, centroids = cv.connectedComponentsWithStats(opened)
+print(f"Number of objects: {n}")
+
+output = img.copy()
+
+for i in range(n):
+    x = stats[i, cv.CC_STAT_LEFT]
+    y = stats[i, cv.CC_STAT_TOP]
+    w = stats[i, cv.CC_STAT_WIDTH]
+    h = stats[i, cv.CC_STAT_HEIGHT]
+    cv.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
 cv.imshow("thresh", thresh)
-cv.imshow("erosion", erosion)
-cv.imshow("img", img)
+cv.imshow("closed", closed)
+cv.imshow("opened", opened)
+cv.imshow("output", output)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
